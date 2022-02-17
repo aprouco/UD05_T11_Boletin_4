a1acb201ee3ce4b7f538b49481ec2143072910dd
aprouco <adripenasrouco@gmail.com>
2022-02-17 13:19:13 +0100

    ejercicio6

---
+++ ejercicio6.html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio6</title>
+</head>
+<body>
+    <?php 
+        $m = 0;
+        do {
+            $mult = $m * 20;
+            $result = 320 - $mult;
+            echo "<p>$result</p>";
+            $m = $m + 1;
+        } while ($m <= 8);
+    ?>
+</body>
+</html>
