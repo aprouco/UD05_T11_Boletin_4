ae80959af6002dd8259bd439b28aa905fe0325df
aprouco <adripenasrouco@gmail.com>
2022-02-21 09:55:04 +0100

    ejercicio7

--- ejercicio7.html.php
+++ ejercicio7.html.php
@@ -8,21 +8,28 @@
 </head>
 <body>
     <form action="<?php echo $_SERVER ['PHP_SELF'];?>" method="post">
+    <input type="hidden" name="intentos" value="1">
     <label for="combinacion">Combinación</label>
     <input type="number" name="combinacion"><br>
     <input type="submit" name="Enviar">
     <?php 
         $combinacionSecreta = "1111";
 
-        if (isset($_POST['combinacion']) && filter_var($_POST['combinacion'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/[0-9]{4}/")))) {
-            if ($_POST['combinacion']==$combinacionSecreta) {
-                echo "<p>La caja fuerte se ha abierto satisfactoriamente</p>";
+        if (isset($_POST['intentos']) && $_POST['intentos'] > 4) {
+            
+            if (isset($_POST['combinacion']) && filter_var($_POST['combinacion'], FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/[0-9]{4}/")))) {
+                if ($_POST['combinacion']==$combinacionSecreta) {
+                    echo "<p>La caja fuerte se ha abierto satisfactoriamente</p>";
+                }else{
+                    echo "<p>Lo siento, esta no es la combinación</p>";
+                }
             }else{
-                echo "<p>Lo siento, esta no es la combinación</p>";
+                echo "<p>Introduce un número de catro díxitos.</p>";
             }
         }else{
-            echo "<p>Introduce un número de catro díxitos.</p>";
+            echo "<p>Ha superado el númeor de intentos</p>";
         }
+
     ?>
     </form>
 
