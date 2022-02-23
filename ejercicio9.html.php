5c0d76d37c1ab5f2624d41c89211ac5f8668863c
aprouco <adripenasrouco@gmail.com>
2022-02-23 14:24:48 +0100

    ejercicio9

---
+++ ejercicio9.html.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ejercicio9</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+
+        <p>Introduce un número: <input type="text" name="num"></p>
+        <input type="submit" value="Enviar">
+
+    </form>
+    <?php 
+
+        $num = $_POST['num'];
+        $longnumero = strlen($num);
+        echo $longnumero;
+
+    ?>
+</body>
+</html>
