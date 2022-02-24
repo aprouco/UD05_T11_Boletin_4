8b023313d202bf006b0f20bf0de4824cd84498e5
aprouco <adripenasrouco@gmail.com>
2022-02-24 11:42:04 +0100

    ejercicio9

--- ejercicio9.html.php
+++ ejercicio9.html.php
@@ -18,7 +18,7 @@
         $num = $_POST['num'];
         if(isset($_POST['num'])){
             $longnumero = strlen($num);
-            echo $longnumero;
+            echo "La cantidad de dígitos del número son = <b>".$longnumero."</b>";
         }else{
             echo "<p>Introduce un número</p>";
         }
