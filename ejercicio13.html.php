e4bc4320c5cccf994de73535ce464059def1f0f5
aprouco <adripenasrouco@gmail.com>
2022-02-25 09:11:21 +0100

    ejercicio13

--- ejercicio13.html.php
+++ ejercicio13.html.php
@@ -11,38 +11,40 @@ session_start();
     <title>Ejercicio13</title>
 </head>
 <body>
-    <form action="<?php echo $_SERVER['PHP_SELF'];?>">
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
     <p>Introduce un número: <input type="number" name="num"></p>
+    <p><input type="submit" value="Enviar"></p>
     </form>
     <?php 
-    ################---------NO VA--------##################
     error_reporting(E_ERROR);
         if(!isset($_SESSION['pos']) && !isset($_SESSION['neg'])){
             $_SESSION['pos']=0;
             $_SESSION['neg']=0;            
         }
 
-        if($_SESSION['neg']+$_SESSION['pos'] < 11){
-            if($_POST['num'] < 0){
+        if($_SESSION['neg']+$_SESSION['pos'] < 10){
+            if(intval($_POST['num']) < 0){
                 $_SESSION['neg']=$_SESSION['neg']+1;
             }else {
                 $_SESSION['pos']=$_SESSION['pos']+1;
             }
-        }
-        echo "<p>Los números positivos son = <b>".$_SESSION['pos']."</b></p>";
-        echo "<p>Los números negativos son = <b>".$_SESSION['neg']."</b></p>";
-        #Destruimos la sesión#
-        $_SESSION = array();
+        }else{
+            echo "<p>Los números positivos son = <b>".$_SESSION['pos']."</b></p>";
+            echo "<p>Los números negativos son = <b>".$_SESSION['neg']."</b></p>";
+            #Destruimos la sesión#
+            $_SESSION = array();
 
-        if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-        );
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+                );
+            }
+            // Finalmente, destruir la sesión.
+            session_destroy();    
         }
-        // Finalmente, destruir la sesión.
-        session_destroy();
+        
     ?>
 </body>
 </html>
