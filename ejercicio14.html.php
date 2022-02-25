65736de597417e9ef0ab71f8f8f83b162a8d1df7
aprouco <adripenasrouco@gmail.com>
2022-02-25 09:25:35 +0100

    ejercicio14

---
+++ ejercicio14.html.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio14</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <p>Introduce la base: <input type="text" name="base"></p>
+        <p>Introduce el exponente: <input type="text" name="exp"></p>
+        <p><input type="submit" value="Enviar"></p>
+    </form>
+    <?php 
+        error_reporting(E_ERROR);
+        $base=$_POST['base'];
+        $exp=$_POST['exp'];
+        if (!filter_var($_POST['base'],FILTER_VALIDATE_INT) || !filter_var($_POST['exp'],FILTER_VALIDATE_INT)) {
+            echo "Debes introducir ambos valores";
+        }else{
+            $pot= pow($base, $exp);
+            echo "La potencia es = ".$pot;
+        }
+    ?>
+</body>
+</html>
