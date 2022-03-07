d7ccaa125ba8d88605dca2bd2b5564206237c2fb
aprouco <adripenasrouco@gmail.com>
2022-03-07 10:34:30 +0100

    ejercicio16

---
+++ ejercicio16.html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio16</title>
+</head>
+<body>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+        <p>Introduce un número: <input type="text" name="num"></p>
+
+        <?php
+
+
+
+        ?>
+    </form>
+</body>
+</html>
