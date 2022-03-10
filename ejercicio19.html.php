be06ae8e2b5efed2ef4b2fc16db98f281460d8d6
aprouco <adripenasrouco@gmail.com>
2022-03-10 13:50:59 +0100

    ejercicio19

---
+++ ejercicio19.html.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio19</title>
+</head>
+<body>
+    
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
+
+        <p>Introduce el valor de la altura: <input type="text" name="altura"></p>
+        <label for="figuras">Selecciona las figuras con las que se creará la pirámide: </label>
+        <select name="select">
+            <option value="ladrillo">Ladrillo</option>
+            <option value="bolas">Bolas</option>
+            <option value="triangulo">Triángulo</option>
+            <option value="asteriscos" selected>Asteriscos</option>
+        </select>   
+
+        <?php
+            #####Pirámide#####
+        ?>
+
+    </form>
+
+</body>
+</html>
